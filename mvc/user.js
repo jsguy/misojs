23a95988661eb0067da6ed80b71cbbf3cc2ae15c
jsguy <mikkel@pointful.com>
2015-01-21 09:36:05 +1100

    Start on validator work

--- mvc/user.js
+++ mvc/user.js
@@ -1,4 +1,5 @@
 var store = require('../server/store.js')(this),
+	validator = require('validator'),
 	miso = require('../server/miso.util.js'),
 	m = require('mithril'),
 	sugartags = require('../server/mithril.sugartags.node.js')(m);
@@ -22,11 +23,35 @@ module.exports.index = {
 	}
 };
 
+var isNotEmpty = function(value){
+	return typeof value !== "undefined" && value !== "" && value !== null;
+}
+
 //	Edit user
 module.exports.edit = {
 	model: function(data){
-		this.name = m.p(data.name);
-		this.id = m.p(data.id);
+		var self = this;
+		self.name = m.p(data.name);
+		self.email = m.p(data.email);
+		self.id = m.p(data.id);
+		
+		//	Returns object with each filed, with true for each valid field,
+		//	or an error messages for each invalid field.
+		this.validateModel = function(){
+			// return {
+			// 	email: isNotEmpty() && validator.isEmail(self.email())? true: "Must be a valid email address",
+			// }
+			return {
+				name: {
+					'required': "You must enter a name"
+				},
+				email: {
+					'required': "You must enter an email address",
+					'email': "Must be a valid email address"
+				}
+			}
+		};
+
 		return this;
 	},
 	controller: function(params) {
