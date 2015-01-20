9ee01981438986f02fd2283788b87a14b2a6243b
jsguy <mikkel@pointful.com>
2015-01-20 18:25:37 +1100

    Fix todo model

--- client/miso.js
+++ client/miso.js
@@ -132,38 +132,16 @@ module.exports.index = {
 	},
 	controller: function(params) {
 		var ctrl = this;
-
-		var model = this.model = new function() {
-			var self = this;
-			self.todos = m.p([
-				{ text: "learn mithril", done: m.p(true) },
-	      		{ text: "build a mithril app", done: m.p(false) }
-	      	]);
-	      	self.input = m.p("");
-			self.left = function(){
-				var count = 0;
-				self.todos().map(function(todo) {
-					count += todo.done() ? 0 : 1;
-				});
-				return count;
-			};
-			self.archive = function(){
-				var list = [];
-				self.todos().map(function(todo) {
-					if(!todo.done()) { list.push(todo); }
-				});
-				self.todos(list);
-			};
-		}();
+		ctrl.model = new module.exports.index.model();
 
 		this.addTodo = function(){
-			var value = model.input();
+			var value = ctrl.model.input();
 			if(value) {
 				console.log('add', value);
-				//	Using bindings model push for arrays
-				model.todos.push({text: model.input(), done: m.p(false)});
-				model.input("");
-				store.save('user', model);
+				//	Using bindings ctrl.model push for arrays
+				ctrl.model.todos.push({text: ctrl.model.input(), done: m.p(false)});
+				ctrl.model.input("");
+				store.save('user', ctrl.model);
 			}
 			return false;
 		};
