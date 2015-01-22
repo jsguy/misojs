e18864da03badde8da285ab3017ed7be9a5335f1
jsguy <mikkel@pointful.com>
2015-01-23 08:26:24 +1100

    Fix nodemon CSS file issue, and add installation details

--- mvc/home.js
+++ mvc/home.js
@@ -5,6 +5,7 @@ var m = require('mithril'),
 module.exports.index = {
 	controller: function(){
 		this.installButton = "Install miso now";
+		this.installButtonLink = "#install";
 		this.introText = "Create apps faster than ever before";
 		return this;
 	},
@@ -15,9 +16,26 @@ module.exports.index = {
 					DIV({ class: "introText" }, ctrl.introText ),
 					BUTTON({ class: "installButton" }, ctrl.installButton ),
 				]),
-				UL([
-					LI(A({ href: '/user/1', config: m.route}, "User edit example")),
-					LI(A({ href: '/todos', config: m.route}, "Todos example"))
+				DIV({ class: "box" }, [
+					H2(A({name: "installation", class: "heading"},"Installation") ),
+					P("To install miso, use npm:"),
+					PRE({ class: "javascript" },[
+						CODE("npm install misojs -g")
+					])
+				]),
+				DIV({ class: "box" }, [
+					H2(A({name: "gettingstarted", class: "heading"},"Getting started") ),
+					P("To create and run a new app in the current directory:"),
+					PRE({ class: "javascript" },[
+						CODE("miso -n myApp\ncd myApp\nmiso run")
+					]),
+					P("Congratulations, you are now running your very own miso app!")
+				]),
+				DIV({ class: "box" }, [
+					UL([
+						LI(A({ href: '/user/1', config: m.route}, "User edit example")),
+						LI(A({ href: '/todos', config: m.route}, "Todos example"))
+					])
 				])
 			]);
 		}
