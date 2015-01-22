8c41830073eae8f633d5c9f569363ffa6d670cd4
jsguy <mikkel@pointful.com>
2015-01-22 11:46:01 +1100

    Add ignore in nodemon, start on store-model-adaptor

--- server/store.js
+++ server/store.js
@@ -1,6 +1,20 @@
 var fs = require('fs'),
 	miso = require('../server/miso.util.js');
 
+
+/*
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var Cat = mongoose.model('Cat', { name: String });
+
+var kitty = new Cat({ name: 'Zildjian' });
+kitty.save(function (err) {
+  if (err) // ...
+  console.log('meow');
+});
+*/
+
 //	Simulated store
 //	TODO: This should interact with the API once we have it.
 module.exports = function(scope) {
